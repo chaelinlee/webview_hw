fa48f5b24e18d2812a4d72bfffadb612734d9100
Leechaelin <elisa0511@naver.com>
2017-05-10 14:24:22 +0900

    thrid

--- app/src/main/java/com/example/leechaelin/webview_hw/MainActivity.java
+++ app/src/main/java/com/example/leechaelin/webview_hw/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.leechaelin.webview_hw;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
@@ -11,24 +16,56 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ProgressDialog dialog;
     ListView listview;
     WebView webview;
     EditText edittext;
-    Button b1;
+    LinearLayout l;
+    ArrayList<Data> data = new ArrayList<Data>();
+    ArrayAdapter<Data>adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        l = (LinearLayout)findViewById(R.id.linear1);
         edittext=(EditText)findViewById(R.id.editText);
         webview=(WebView)findViewById(R.id.webView);
         dialog= new ProgressDialog(this);
+        listview = (ListView)findViewById(R.id.listview);
+        adapter = new ArrayAdapter<Data>(this,android.R.layout.simple_list_item_1,data);
+        listview.setAdapter(adapter);
 
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                AlertDialog.Builder dlg= new AlertDialog.Builder(MainActivity.this);
+                dlg.setMessage("정말로 삭제하시겠습니까? ")
+                        .setNegativeButton("닫기",null)
+                        .setPositiveButton("확인 ", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                data.remove(position);
+                                adapter.notifyDataSetChanged();
+                                Toast.makeText(getApplicationContext(),"삭제하였습니다.",Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+
+                return false;
+            }
+        });
         webview.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
@@ -49,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
         webview.setWebChromeClient(new WebChromeClient(){
             @Override
             public void onProgressChanged(WebView view, int newProgress) {
@@ -62,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
                 return super.onJsAlert(view, url, message, result);
             }
         });
+
         WebSettings webSettings= webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setBuiltInZoomControls(true);
@@ -73,4 +112,23 @@ public class MainActivity extends AppCompatActivity {
         String url=edittext.getText().toString();
         webview.loadUrl(url);
     }
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.add:
+                webview.setVisibility(View.VISIBLE);
+                listview.setVisibility(View.INVISIBLE);
+                break;
+            case R.id.list:
+                webview.setVisibility(View.INVISIBLE);
+                listview.setVisibility(View.VISIBLE);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
